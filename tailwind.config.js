3e377aea8341807c02358c12ba3a27bc82cdb0a8
Palo23 <lmarespinortizz23@gmail.com>
2024-09-11 16:31:54 -0600

    refactor: add theme for fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import { colors, fonts } from './src/styles';
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors,
+      fontSize: fonts,
+    },
   },
   plugins: [],
 };
