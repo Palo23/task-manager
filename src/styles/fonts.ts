9b94dc14a983accf0f7d081a7f38bd2b42be1164
Palo23 <lmarespinortizz23@gmail.com>
2024-09-11 16:31:45 -0600

    feat: add font configs

---
+++ src/styles/fonts.ts
@@ -0,0 +1,67 @@
+const fonts = {
+  'display-xl': [
+    '64px',
+    {
+      lineHeight: '88px',
+      letterSpacing: '1px',
+    },
+  ],
+  'display-l': [
+    '56px',
+    {
+      lineHeight: '72px',
+      letterSpacing: '1px',
+    },
+  ],
+  'display-m': [
+    '48px',
+    {
+      lineHeight: '56px',
+      letterSpacing: '1px',
+    },
+  ],
+  'display-s': [
+    '32px',
+    {
+      lineHeight: '48px',
+      letterSpacing: '1px',
+    },
+  ],
+  'display-xs': [
+    '24px',
+    {
+      lineHeight: '32px',
+      letterSpacing: '1px',
+    },
+  ],
+  'body-xl': [
+    '20px',
+    {
+      lineHeight: '32px',
+      letterSpacing: '0.75px',
+    },
+  ],
+  'body-l': [
+    '18px',
+    {
+      lineHeight: '32px',
+      letterSpacing: '0.75px',
+    },
+  ],
+  'body-m': [
+    '15px',
+    {
+      lineHeight: '24px',
+      letterSpacing: '0.75px',
+    },
+  ],
+  'body-s': [
+    '13px',
+    {
+      lineHeight: '22px',
+      letterSpacing: '0.25px',
+    },
+  ],
+};
+
+export { fonts };
