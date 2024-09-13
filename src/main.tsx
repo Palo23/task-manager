3045be1e6d6fa022c73bbe67873e0984f107bbb9
Palo23 <lmarespinortizz23@gmail.com>
2024-09-13 11:31:45 -0600

    refactor: add router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { RoutesProvider } from './providers/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RoutesProvider />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
