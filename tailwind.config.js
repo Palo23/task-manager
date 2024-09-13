1bfe28f0a510146440613fd20b324c6f4c5a106a
Palo23 <lmarespinortizz23@gmail.com>
2024-09-13 14:47:28 -0600

    refactor: add gradient to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'li-gradient': 'linear-gradient(90deg, rgba(186, 37, 37, 0) 0%, rgba(210, 77, 77, 0.1) 100%)',
+      },
       fontSize: fonts,
       borderRadius: {
         lg: 'var(--radius)',
