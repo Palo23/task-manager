27d715e9502111e80c7d400616a8fe436a5d6ed9
Palo23 <lmarespinortizz23@gmail.com>
2024-09-13 12:46:43 -0600

    refactor: update app main styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import { RoutesProvider } from './providers/index.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RoutesProvider />
-    <App />
   </StrictMode>
 );
