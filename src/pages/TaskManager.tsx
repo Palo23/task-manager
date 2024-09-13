5b0d3c56f26a9a877405663384570007684fcee8
Palo23 <lmarespinortizz23@gmail.com>
2024-09-13 12:48:55 -0600

    refactor: create main structure and add navigation bar

--- src/pages/TaskManager.tsx
+++ src/pages/TaskManager.tsx
@@ -1,7 +1,15 @@
+import { TopNavigationBar } from '@/components';
+
 const TaskManager = () => {
   return (
-    <div>
-      <h1>Task Manager</h1>
+    <div className="w-full flex flex-row p-6">
+      <div className="w-1/4">
+        {/* TODO: Add sidebar */}
+        <h1>Sidebar</h1>
+      </div>
+      <div className="w-full lg:w-3/4">
+        <TopNavigationBar />
+      </div>
     </div>
   );
 };
