509d71c5e8fdd34e8e00aa05601f196674b387e0
Palo23 <lmarespinortizz23@gmail.com>
2024-09-13 12:48:43 -0600

    refactor: add new components to navigation bar

---
+++ src/components/TopNavigationBar.tsx
@@ -0,0 +1,28 @@
+import { CiSearch } from 'react-icons/ci';
+import { CgBell } from 'react-icons/cg';
+import { colors } from '@/styles';
+import { Images } from '@/lib';
+import { useState } from 'react';
+import { AvatarComponent, InputClear } from '.';
+
+const TopNavigationBar = () => {
+  const [value, setValue] = useState('');
+  return (
+    <div className="flex flex-row items-center justify-between bg-neutral-4 rounded-2xl py-2 px-3 gap-4">
+      <CiSearch color={colors['neutral-2']} size={24} />
+      <InputClear
+        value={value}
+        onChange={setValue}
+        className="text-neutral-2"
+        inputProps={{
+          type: 'search',
+          placeholder: 'Search',
+        }}
+      />
+      <CgBell color={colors['neutral-2']} size={24} />
+      <AvatarComponent src={Images.avatar} alt="RV" className="object-cover h-10 w-10" />
+    </div>
+  );
+};
+
+export { TopNavigationBar };
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
+export * from './AvatarComponent';
 export * from './ErrorBoundary';
+export * from './InputClear';
+export * from './TopNavigationBar';
 export * from './ui';
