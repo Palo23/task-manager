fac9407abfc3995745ac43524d0f19db07c212a7
Palo23 <lmarespinortizz23@gmail.com>
2024-09-14 16:11:22 -0600

    feat: create card ui component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './accordion';
 export * from './avatar';
 export * from './button';
+export * from './card';
 export * from './input';
 export * from './table-title';
 export * from './task-view-theader';
